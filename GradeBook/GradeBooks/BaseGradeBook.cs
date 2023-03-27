20b3153f826bf3e12a54be52dca094d178782b8e
OlafGrzesiak <olafgrzesiak0611@gmail.com>
2023-03-27 12:47:34 +0200

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,12 +14,15 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public GradeBookType Type { get; set; }
         public List<Student> Students { get; set; }
+
         public bool IsWeighted { get; set; }
+
+
         public BaseGradeBook(string name, bool isWeighted)
         {
+            IsWeighted = isWeighted;
             Name = name;
             Students = new List<Student>();
-            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
